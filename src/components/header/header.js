da351ed50b487e0d336215cf19f49ab56dd2b925
Roman-Denysenko <roman.denisenko83@gmail.com>
2021-08-31 21:17:28 +0300

    Add films in nav

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import s from './Header.module.css'
+import s from './Header.module.css';
 
 const Header = () => {
   return (
     <header className={s.header}>
       <nav>
         <a>news</a>
+        <a>films</a>
       </nav>
     </header>
   );
